bbfaa484c5f91bd154224f2e7e7a4ffe13e40a6e
Vidur <vidurkhanal@outlook.com>
2020-11-10 08:26:41 +0545

    HELLO WORLD ADDED

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('hi')
+import discord from "discord.js";
+import "dotenv/config";
+
+const client = new discord.Client();
+client.login(process.env.DISCORD_BOT_TOKEN);
+
+const DiscordIsReady = () => {
+  console.log("DISCORD CONNECTED");
+};
+
+client.on("ready", DiscordIsReady);
+
+client.on("message", async (msg) => {
+  if (
+    msg.channel.id == "775536407866376207" &&
+    msg.content.toLowerCase() === "hello"
+  ) {
+    // await msg.react(":)")
+    await msg.channel.send("Hillo");
+  }
+});
